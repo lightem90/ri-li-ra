ddcaf36ad483a89a420bb58b0437044badd03ffb
lightem90 <matteo.liera@gmail.com>
2020-03-21 20:51:33 +0100

    fix api key, installed firebase and angular material (not working)

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,10 +1,12 @@
 export const environment = {
    production: true,
    firebase: {
+      apiKey: "AIzaSyC2rTtyQeTfSvVIfMdP6pObrVbv4b7AAiA",
       authDomain: "ri-li-44726.firebaseapp.com",
       databaseURL: "https://ri-li-44726.firebaseio.com",
       projectId: "ri-li-44726",
       storageBucket: "",
       messagingSenderId: "196948006879",
+      appId: "1:196948006879:web:5a9b8b87cd4bea26c8566a"
    }
 };
