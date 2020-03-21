4bbb7efee129dc1d2de53fb8509aedd0b9e8c970
lightem90 <matteo.liera@gmail.com>
2020-03-21 21:48:46 +0100

    working firebase

--- src/app/core/services/firebase-helper.ts
+++ src/app/core/services/firebase-helper.ts
@@ -7,7 +7,7 @@ import { AngularFireStorageModule } from '@angular/fire/storage';
 @Injectable()
 export class FirebaseHelper
 {
-  firebaseRefs : any
+  firebaseRefs : firebase.database.Reference[]
   /**
    * Initializes this Firebase facade.
    * @constructor
@@ -17,7 +17,6 @@ export class FirebaseHelper
     private database: AngularFireDatabase,
     private storage: AngularFireStorageModule) 
   {
-
     // Firebase references that are listened to.
     this.firebaseRefs = [];
   }
@@ -28,5 +27,45 @@ export class FirebaseHelper
   cancelAllSubscriptions() {
     this.firebaseRefs.forEach((ref) => ref.off());
     this.firebaseRefs = [];
+  }
+
+  test(){
+    this.updateDataForUser('aa', 'test', {aa: 'aa'}, 'tt')
+  }
+
+  updateDataForUser(
+    userId: string,
+    dataKey: string, 
+    dataToAdd: any, 
+    dataTableName: string,
+    relationTableName: string = "",
+    doubleIndex : boolean = false)
+  {
+      var updates = {};
+      updates['/' + dataTableName + '/' + dataKey] = dataToAdd;
+      
+      if (relationTableName != "") {
+        if (doubleIndex){
+          updates['/' + relationTableName + '/' + userId + '/' + dataKey] = dataToAdd
+          }
+          else {
+          updates['/' + relationTableName + '/' + userId] = dataToAdd
+          }
+      }
+
+      return this.database.database.ref().update(updates);
+  }
+
+  //adds into a table a new document and if any a relation table for selected user
+  addDataForUser(
+    userId: string, 
+    dataToAdd: any, 
+    dataTableName: string,
+    relationTableName: string = "")
+  {
+    var newDataKey = this.database.database.ref().child(dataTableName).push().key;
+    dataToAdd.id = newDataKey;
+
+    return this.updateDataForUser(userId, newDataKey, dataToAdd, dataTableName, relationTableName)
   }
 }
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -12,6 +12,6 @@ export class HelloComponent  {
 
   constructor(private tt : FirebaseHelper)
   {
-
+    this.tt.test()
   }
 }
