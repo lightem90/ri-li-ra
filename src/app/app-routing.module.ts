1aa47afe39955da7b8062163d04a6e404e1123ca
lightem90 <matteo.liera@gmail.com>
2020-03-22 12:21:54 +0100

    home page components created and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,21 @@ import { Routes, RouterModule, CanActivateChild } from '@angular/router';
 
 import { LoginComponent } from './static/login/login.component';
 import { RegisterComponent } from './static/register/register.component';
-import { NotFoundComponent } from './static/register/not-found.component';
+import { NotFoundComponent } from './static/not-found/not-found.component';
+import { AboutComponent } from './static/about/about.component';
+import { ContactsComponent } from './static/contacts/contacts.component';
+import { FaqComponent } from './static/faq/faq.component';
+import { ConfiguratorComponent } from './configurator/configurator.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },  
   { path: 'not-found', component: NotFoundComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'contacts', component: ContactsComponent},
+  { path: 'faq', component: FaqComponent},
+  { path: 'configurator', component: ConfiguratorComponent},
   { path: '', component: AppComponent},
   // { path: '**', redirectTo: '/not-found'}
   { path: '**', redirectTo: '/not-found'} // MUST BE LAST, IT IDENTIFIES ALL PATHS!
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 
+import { AppRoutingModule } from './app-routing.module';
 import { MatSliderModule } from '@angular/material/slider';
 import { environment } from './environments/environment';
 
@@ -19,6 +20,11 @@ import * as firebase from 'firebase/app';
 import { LoginComponent } from './static/login/login.component';
 import { RegisterComponent } from './static/register/register.component';
 import { NotFoundComponent } from './static/not-found/not-found.component';
+import { AboutComponent } from './static/about/about.component';
+import { FaqComponent } from './static/faq/faq.component';
+import { ContactsComponent } from './static/contacts/contacts.component';
+import { HeaderComponent } from './header/header.component';
+import { ConfiguratorComponent } from './configurator/configurator.component';
 
 if (!firebase.apps.length) {
     firebase.initializeApp(environment.firebase);
@@ -32,8 +38,19 @@ if (!firebase.apps.length) {
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireStorageModule,
     AngularFireAuthModule,
-    AngularFireDatabaseModule    ],
-  declarations: [ AppComponent, HelloComponent, LoginComponent, RegisterComponent, NotFoundComponent ],
+    AngularFireDatabaseModule,
+    AppRoutingModule    ],
+  declarations: [ 
+    AppComponent, 
+    HelloComponent, 
+    LoginComponent, 
+    RegisterComponent, 
+    NotFoundComponent, 
+    AboutComponent, 
+    FaqComponent, 
+    ContactsComponent, 
+    HeaderComponent, 
+    ConfiguratorComponent ],
   bootstrap:    [ AppComponent ],
   providers: [FirebaseHelper]
 })
