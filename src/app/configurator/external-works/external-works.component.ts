461441edd83faa1ef3dada228db40e9aada29a6b
lightem90 <matteo.liera@gmail.com>
2020-03-30 17:14:02 +0200

    ri-aggiunti componenti

---
+++ src/app/configurator/external-works/external-works.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-external-works',
+  templateUrl: './external-works.component.html',
+  styleUrls: ['./external-works.component.css']
+})
+export class ExternalWorksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/configurator/recap/recap.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recap',
+  templateUrl: './recap.component.html',
+  styleUrls: ['./recap.component.css']
+})
+export class RecapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() 
+
+}
