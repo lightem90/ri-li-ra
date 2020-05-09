fd777aa8e0af23c1a9b99f42b0bc859bdfee78c9
lightem90 <matteo.liera@gmail.com>
2020-05-09 16:33:13 +0200

    added inline editor

---
+++ src/app/shared/inline-edit/inline-edit.component.ts
@@ -0,0 +1,89 @@
+import { Component,
+  Input,
+  ElementRef,
+  ViewChild,
+  Renderer2,
+  forwardRef,
+  OnInit } from '@angular/core';
+  import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+
+const INLINE_EDIT_CONTROL_VALUE_ACCESSOR = {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => InlineEditComponent),
+    multi: true
+  };
+
+//from: https://www.codementor.io/@godson.ukpere/creating-an-inline-edit-component-in-angular-2-nmkdlpxtq
+@Component({
+  selector: 'app-inline-edit',
+  templateUrl: './inline-edit.component.html',
+  providers: [INLINE_EDIT_CONTROL_VALUE_ACCESSOR],
+  styleUrls: ['./inline-edit.component.css']
+})
+
+export class InlineEditComponent implements ControlValueAccessor, OnInit {
+
+  @ViewChild('inlineEditControl') inlineEditControl; // input DOM element
+  @Input() label: string = '';  // Label value for input element
+  @Input() type: string = 'text'; // The type of input element
+  @Input() required: boolean = false; // Is input requried?
+  @Input() disabled: boolean = false; // Is input disabled?
+  private _value: string = ''; // Private variable for input value
+  private preValue: string = ''; // The value before clicking to edit
+  private editing: boolean = false; // Is Component in edit mode?
+  public onChange: any = Function.prototype; // Trascend the onChange event
+  public onTouched: any = Function.prototype; // Trascend the onTouch event
+
+  constructor(element: ElementRef, private _renderer: Renderer2) {
+  }
+
+  ngOnInit() {
+  }
+
+  // Control Value Accessors for ngModel
+  get value(): any {
+    return this._value;
+  }
+
+  set value(v: any) {
+    if (v !== this._value) {
+      this._value = v;
+      this.onChange(v);
+    }
+  }
+
+  // Do stuff when the input element loses focus
+  onBlur($event: Event) {
+    this.editing = false;
+  }
+
+  // Start the editting process for the input element
+  edit(value) {
+    if (this.disabled) {
+      return;
+    }
+
+    this.preValue = value;
+    this.editing = true;
+    // Focus on the input element just as the editing begins
+    setTimeout(_ => this._renderer
+      .selectRootElement(this.inlineEditControl).focus()
+    );
+  }
+
+  // Required for ControlValueAccessor interface
+  writeValue(value: any) {
+    this._value = value;
+  }
+
+  // Required forControlValueAccessor interface
+  public registerOnChange(fn: (_: any) => {}): void {
+    this.onChange = fn;
+  }
+
+  // Required forControlValueAccessor interface
+  public registerOnTouched(fn: () => {}): void {
+    this.onTouched = fn;
+  }
+
+}
