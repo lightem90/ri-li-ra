5933a39caa3dac3d3a7ad7342353ca6f4d276bac
lightem90 <matteo.liera@gmail.com>
2020-08-26 11:06:56 +0200

    wip new implementation for invoicing

--- src/app/shared/budget-print/budget-print.component.ts
+++ src/app/shared/budget-print/budget-print.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
 import { ActivatedRoute } from '@angular/router';
 import { AccountManagerService } from '../../core/services/account-manager.service';
 
@@ -32,6 +36,50 @@ export class BudgetPrintComponent implements OnInit {
     //   this._loadingSuccessfull = true
     //   console.log("budget to print found")
     //   }  
+  }
+
+  budgetDefinitio() {
+    let docDefinition =  
+    {
+      content: [
+        {text: 'Tables', style: 'header'},
+        'Official documentation is in progress, this document is just a glimpse of what is possible with pdfmake and its layout engine.',
+        {text: 'A simple table (no headers, no width specified, no spans, no styling)', style: 'subheader'},
+        'The following table has nothing more than a body array',
+        {
+          style: 'tableExample',
+          table: {
+            body: [
+              ['Column 1', 'Column 2', 'Column 3'],
+              ['One value goes here', 'Another one here', 'OK?']
+            ]
+          }
+        }],
+        styles: {
+          header: {
+            fontSize: 18,
+            bold: true,
+            margin: [0, 0, 0, 10]
+          },
+          subheader: {
+            fontSize: 16,
+            bold: true,
+            margin: [0, 10, 0, 5]
+          },
+          tableExample: {
+            margin: [0, 5, 0, 15]
+          },
+          tableHeader: {
+            bold: true,
+            fontSize: 13,
+            color: 'black'
+          }
+        },
+        defaultStyle: {
+          // alignment: 'justify'
+        }
+     }
+     return docDefinition
   }
 
 }
